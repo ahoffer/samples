1bed4bc7f6ab4202a2632ef3744f6af9639365c2
Aaron Hoffer <howard.hoffer@gmail.com>
2025-11-16 05:20:32 -0800

    added helper script

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM bluenviron/mediamtx:1.15.3-ffmpeg
 
 WORKDIR /app
 
+# Copy stream wrapper script
+COPY stream-video.sh /usr/local/bin/stream-video.sh
+RUN chmod +x /usr/local/bin/stream-video.sh
+
 # Bake videos into the image
 COPY videos /app/videos
 
---
+++ stream-video.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Wrapper script to stream a video file to MediaMTX via RTSP
+# Usage: stream-video.sh <video-file> <stream-path>
+
+VIDEO_FILE="$1"
+STREAM_PATH="$2"
+
+exec ffmpeg -re -stream_loop -1 \
+  -i "$VIDEO_FILE" \
+  -c:v libx264 -profile:v baseline -preset ultrafast \
+  -map 0 \
+  -f rtsp "rtsp://localhost:8554/$STREAM_PATH"
